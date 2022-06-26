951a1d9a8a5a34892a470e3cab03ddf68bc39cec
OmidErnesto <ochahuaris@unsa.edu.pe>
2022-06-26 18:18:08 -0500

    Funciones or y xor implementadas

--- ejercicio1.c
+++ ejercicio1.c
@@ -6,28 +6,28 @@ int bic(int x, int m);
 
 /* Calcular x|y usando solo llamadas a las funciones bis y bic */
 
-/*int bool_or(int x, int y) {
+int bool_or(int x, int y) {
 
- int resultado = __________ ;
+ int resultado = bis(x,y);
 
  return resultado;
 
-}*/
+}
 
 /* Calcula x^y usando solo llamadas a las funciones bis y bic */
 
-/*int bool_xor(int x, int y) {
+int bool_xor(int x, int y) {
 
- int resultado = __________;
+ int resultado = bic(bis(x,y), bic(x,y)^x);
 
  return resultado;
 
-}*/
+}
 
 int main(){
 
-	printf("%d\n",bis(13,6));
-	printf("%d\n",bic(13,6));
+	printf("%d\n",bool_or(13,6));
+	printf("%d\n",bool_xor(13,6));
 	return 0;
 }
 
