8dd95c455ff7e722d225b9f866acb593ab1a0d8b
OmidErnesto <ochahuaris@unsa.edu.pe>
2022-06-26 16:44:31 -0500

    Funciones bis y bic implementadas

--- ejercicio1.c
+++ ejercicio1.c
@@ -1,34 +1,46 @@
+#include <stdio.h> 
+
 int bis(int x, int m);
 
 int bic(int x, int m);
 
 /* Calcular x|y usando solo llamadas a las funciones bis y bic */
 
-int bool_or(int x, int y) {
+/*int bool_or(int x, int y) {
 
  int resultado = __________ ;
 
  return resultado;
 
-}
+}*/
 
 /* Calcula x^y usando solo llamadas a las funciones bis y bic */
 
-int bool_xor(int x, int y) {
+/*int bool_xor(int x, int y) {
 
  int resultado = __________;
 
  return resultado;
 
-}
+}*/
 
 int main(){
 
+	printf("%d\n",bis(13,6));
+	printf("%d\n",bic(13,6));
+	return 0;
 }
 
 int bis(int x, int m) {
 
- unsigned int Z = x | 1<<m;
+ unsigned int Z = x | m;
+ return Z;
+
+}
+
+int bic(int x, int m) {
+
+ unsigned int Z = (x | m) ^ m;
  return Z;
 
 }
