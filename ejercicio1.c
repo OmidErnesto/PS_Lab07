717b652d759e24785c198c682de16cdba5a72393
OmidErnesto <ochahuaris@unsa.edu.pe>
2022-06-26 14:16:42 -0500

    Codigo base

---
+++ ejercicio1.c
@@ -0,0 +1,23 @@
+int bis(int x, int m);
+
+int bic(int x, int m);
+
+/* Calcular x|y usando solo llamadas a las funciones bis y bic */
+
+int bool_or(int x, int y) {
+
+ int resultado = __________ ;
+
+ return resultado;
+
+}
+
+/* Calcula x^y usando solo llamadas a las funciones bis y bic */
+
+int bool_xor(int x, int y) {
+
+ int resultado = __________;
+
+ return resultado;
+
+}
