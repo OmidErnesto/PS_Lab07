7f7e76faf712ae3b2d3b8c2e31f66dd970a685f4
OmidErnesto <ochahuaris@unsa.edu.pe>
2022-06-26 19:28:59 -0500

    Solucion ejercicio 2

--- ejercicio2.c
+++ ejercicio2.c
@@ -1,11 +1,19 @@
 #include <stdio.h> 
+#include <stdint.h>
+#include <inttypes.h>  
 
-int mult_ok(int x, int y) {
+int64_t mult_ok(int64_t x, int64_t y) {
 
- int p = x*y;
+ int64_t p = x*y;
 
- /* x es 0, o dividir p por x = y */
+ /* x es 0, o restar al int64_t maximo p ser mayor que 0*/
 
- return !x || p/x == y;
+ return !x || INT64_MAX - p > 0;
 
+}
+
+int main(){
+
+	printf("%" PRId64 "\n",mult_ok(152123123123153,15316));
+        return 0;
 }
